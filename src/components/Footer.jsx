81119c72a0ca4fe5a352818996ebb1e75d504085
Joe Mutuma <joemutuma2001@gmail.com>
2024-11-27 10:10:04 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import { FaDiscord, FaTwitter, FaYoutube,  FaGithub, FaTwitch } from "react-icons/fa";
+
+const links = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://www.youtube.com/@joecustomz5106", icon: <FaYoutube /> },
+  { href: "https://github.com/joemutuma2001", icon: <FaGithub /> },
+  { href: "https://twitch.com", icon: <FaTwitch /> },
+
+];
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-light md:text-left">
+        
+          ©Joe 2024. All rights reserved
+        </p>
+
+        <div className="flex justify-center gap-4  md:justify-start">
+          {links.map((link) => (
+            <a
+              key={link}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm font-light hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
